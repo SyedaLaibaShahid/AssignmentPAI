e39b8f8156f7204c1e9dff805f67b4b565664f06
Syeda Laiba Shahid <syedalaibashahid2@gmail.com>
2024-09-27 22:32:21 +0500

    First Commit in Main

---
+++ Product_stubs.py
@@ -0,0 +1,13 @@
+#SP23-BAI-052 SYEDA LAIBA SHAHID ,
+#SP23-BAI-022 KHADIJA SAJID
+class Product:
+  def _init_(self, name, amount, price):
+      self.name = name
+      self.amount = amount
+      self.price = price
+
+  def get_price(self, quantity):
+      pass
+
+  def make_purchase(self, quantity):
+      pass
